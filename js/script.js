1a1f4c931de6444c6c10b93154994ed21210529a
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-15 16:08:03 +0100

    inseriti contatti da array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
 const app = new Vue ({
-    el:'#root',
+    el:'#contacts',
     data:{
+        arrContacts: [
+            {
+                name: 'Michele',
+                image:'img/avatar_1.jpg',
+            },
+            {
+                name: 'Fabio',
+                image:'img/avatar_2.jpg',
+            },
+            {
+                name: 'Samuele',
+                image:'img/avatar_3.jpg',
+            },
+            {
+                name: 'Alessandro B.',
+                image:'img/avatar_4.jpg',
+            },
+            {
+                name: 'Alessandro L.',
+                image:'img/avatar_5.jpg',
+            },
+            {
+                name: 'Claudia',
+                image:'img/avatar_6.jpg',
+            },
+            {
+                name: 'Federico',
+                image:'img/avatar_7.jpg',
+            },
+            {
+                name: 'Davide',
+                image:'img/avatar_8.jpg',
+            },
+            
+        ]
 
     },
     methods:{
 
     },
-})
+});
